7e1d9dc0f93198569ac160e1069f60e561afb3a2
teoriaSzyderca <52601119+teoriaSzyderca@users.noreply.github.com>
2019-08-24 22:08:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-wget 
+#!/bin/bash
+
+mkdir -p ~/.vim/colors
+wget https://raw.githubusercontent.com/teoriaSzyderca/configs/master/.vimrc -O ~/.vimrc
+wget https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim -O ~/.vimrc/colors/jellybeans.vim
